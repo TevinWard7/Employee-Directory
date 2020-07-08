6fa0e9549e8b530b6cb58fbe3fd34b76eb573d25
Tevin <tevinward7@gmail.com>
2020-07-08 15:53:49 -0400

    added images to database & added bootstrap card

--- src/components/employeeList/employeeList.js
+++ src/components/employeeList/employeeList.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import Employees from "../../utils/employees.json"
+import "./style.css"
 
 const EmployeeList = () => {
 
@@ -9,20 +10,33 @@ const EmployeeList = () => {
 
     const displayEmployees = () => {
         if (searchName) {
-            return employFiltered.map(employee => <li>{employee.name}</li>)
+            return employFiltered.map(employee => 
+            <div className="card">
+            <img src={employee.image} class="card-img-top" alt="profile" height="10%"/>
+            <div className="card-body">
+            <h5 class="card-title">{employee.name}</h5>
+            </div>
+          </div>
+            )
         } else {
             return employeeState.map(employee => <li>{employee.name}</li>)
         }
     }
 
     return(
-        <>
+
+        <div className="container">
+
         <h3>{searchName}</h3>
         <input value={searchName.charAt(0).toUpperCase() + searchName.slice(1)} placeholder="Name" type="text" name="search" onChange={e => setSearchName(e.target.value)} />
+
+        <br/>
+
         <ul>
             {displayEmployees()}
         </ul>
-        </>
+
+        </div>
     )
 };
 
